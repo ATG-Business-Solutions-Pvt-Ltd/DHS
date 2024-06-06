4d6694a3066466bd84b05ca9a08f98aea186576f
Sheetal Warbhuvan <sheetalwarbhuvan@ATGADMINs-MacBook-Pro.local>
2024-06-06 09:40:31 +0530

    Django rest api for logging a ticket

---
+++ dhs_api/dhs_api/views.py
@@ -0,0 +1,174 @@
+from rest_framework.decorators import api_view
+import requests
+from rest_framework.response import Response
+from rest_framework import status
+import json
+from urllib.error import HTTPError
+from urllib.parse import urlencode
+from urllib.request import urlopen,Request
+import logging
+logger = logging.getLogger(__name__)
+# from drf_spectacular.utils import extend_schema, OpenApiTypes, OpenApiExample, OpenApiRequestBody
+# from drf_spectacular.types import OpenApiTypes
+
+
+@api_view(http_method_names=["POST"])
+def log_ticket(request):
+    access_token=get_refresh_token()
+    logger.info(f'Generating new ticket for provided information.')
+    logger.info(f'{request.data}')
+    res=create_request(access_token,request.data)
+    res=json.loads(res)
+    logger.info(f'Ticket has been generated with display Id {res["request"]["display_id"]}')
+    return Response(data=res,status=status.HTTP_200_OK)
+  
+# @extend_schema(
+#     methods=['POST'],
+#     request=OpenApiRequestBody(
+#         content={
+#             'multipart/form-data': {
+#                 'schema': {
+#                     'type': 'object',
+#                     'properties': {
+#                         'subject': {'type': 'string', 'description': 'Subject'},
+#                         'description': {'type': 'string', 'description': 'Description'},
+#                         'email': {'type': 'string', 'description': 'email'}
+#                     },
+#                     'required': ['subject', 'description', 'email']
+#                 }
+#             }
+#         }
+#     ),
+#     responses={
+#         200: OpenApiTypes.OBJECT,
+#         400: OpenApiTypes.OBJECT
+#     }
+# )
+@api_view(http_method_names=["POST"])
+def log_ticketV2(request):
+    access_token=get_refresh_token()
+    logger.info(f'Generating new ticket for provided information.')
+    logger.info(f'{request.data}')
+    res=create_request_v2(access_token,request.data)
+    res=json.loads(res)
+    if "request" in res:
+      logger.info(f'Ticket has been generated with display Id {res["request"]["display_id"]}')
+    else:
+      logger.info(f'Failed to generate ticket {res} ')
+    return Response(data=res,status=status.HTTP_200_OK)
+  
+
+def get_refresh_token():
+    url = "https://accounts.zoho.in/oauth/v2/token"
+    data ={"Content-Type": "application/x-www-form-urlencoded",
+    "refresh_token" : "1000.36e9e456ca5702a34375d0dfb4145217.8dcad6af9f16bbf6ad3076f91703c80d",
+    "grant_type" : "refresh_token",
+    "client_id" : "1000.XF2DTSF0J2VSBTHI738BZ7LYX95G8H",
+    "client_secret" : "4520676351533e90d15fcb8e86c12eba5334165bce",
+    "redirect_uri" : "https://accounts.zoho.in" }          
+    response = requests.post(url,data=data,verify=False)
+    python_dict = json.loads(response.text)
+    logger.info(f'Authentication is successful')
+    return python_dict.get("access_token")
+
+def create_request(access_token,data):
+    url = "https://sdpondemand.manageengine.in/api/v3/requests"
+    headers ={"Accept": "application/vnd.manageengine.sdp.v3+json", 
+          "Authorization" : f"Zoho-oauthtoken {access_token}", 
+          "Content-Type" : "application/x-www-form-urlencoded"}
+    input_data='''
+    {
+    "request": {
+      "subject": "Test from DHS Server",
+      "group": {
+        "name": "IT SupportCenter"
+      },
+      "requester": {
+        "email_id": "sairam.thummala@deliverhealth.com" 
+      },
+      "impact": {
+        "name": "Minor / Limited"
+      },
+      "request_type": {
+        "name": "Incident"
+      },
+      "priority": {
+        "name": "Normal"
+      },
+      "status": {
+        "name": "OPEN"
+      },
+      "description": "Test from Aeries Server"
+    }
+  
+}
+    
+    '''
+   
+    python_dict = json.loads(data["data"])
+    input_data = json.dumps(python_dict, indent=2)
+    data = urlencode({"input_data":input_data}).encode()
+    httprequest = Request(url, headers=headers,data=data, method="POST")
+    try:
+        with urlopen(httprequest) as response:
+            res=response.read().decode()
+            logger.info(f'Ticket has been generated -> {res}')
+            return res
+    except HTTPError as e:
+        res=e.read().decode()
+        logger.error(f'Failed to generate Ticket -> {res}')
+        return res
+      
+      
+      
+def create_request_v2(access_token,data):
+    url = "https://sdpondemand.manageengine.in/api/v3/requests"
+    headers ={"Accept": "application/vnd.manageengine.sdp.v3+json", 
+          "Authorization" : f"Zoho-oauthtoken {access_token}", 
+          "Content-Type" : "application/x-www-form-urlencoded"}
+    input_data='''
+    {
+    "request": {
+      "subject": "Test from DHS Server",
+      "group": {
+        "name": "IT SupportCenter"
+      },
+      "requester": {
+        "email_id": "sairam.thummala@deliverhealth.com" 
+      },
+      "impact": {
+        "name": "Minor / Limited"
+      },
+      "request_type": {
+        "name": "Incident"
+      },
+      "priority": {
+        "name": "Normal"
+      },
+      "status": {
+        "name": "OPEN"
+      },
+      "description": "Test from Aeries Server"
+    }
+  
+}
+    
+    '''
+   
+    python_dict = json.loads(input_data)
+    python_dict['request']['subject'] = data.get("subject")
+    python_dict['request']['requester']['email_id'] = data.get('email')
+    python_dict['request']['description'] = data.get('description')
+    input_data = json.dumps(python_dict, indent=2)
+    data = urlencode({"input_data":input_data}).encode()
+    httprequest = Request(url, headers=headers,data=data, method="POST")
+    try:
+        with urlopen(httprequest) as response:
+            res=response.read().decode()
+            logger.info(f'Ticket has been generated -> {res}')
+            return res
+    except HTTPError as e:
+        res=e.read().decode()
+        logger.error(f'Failed to generate Ticket -> {res}')
+        return res
+      
