6455951c0b167787b066058cc12f52a12e26e612
saimaryala <sai.maryala@aeriestechnology.com>
2024-06-25 19:04:53 +0530

    rasa bot first comit

--- dhs_api/dhs_history/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DhsHistoryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dhs_history'
--- dhs_api/scheduled_task/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScheduledTaskConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scheduled_task'
